d370d469c3d2b67899135295c3d581410cbd6b37
Habib Opeyemi Adebayo <adebayohabib7@gmail.com>
2025-12-24 00:49:19 +0100

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://fyndam-api.vercel.app",
         changeOrigin: true,
       },
     },
